51cee7a3ac3f285e14c752bdc7eeb073906e3436
bilalnugroho <bilalxr3@gmail.com>
2016-11-20 09:34:40 +0700

    Penataan Grid Iqro'

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl5102030/belajarmengaji/IqroPage.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl5102030/belajarmengaji/IqroPage.java
@@ -4,15 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
+import id.sch.smktelkom_mlg.project.xiirpl5102030.belajarmengaji.adapter.adapterr;
+import id.sch.smktelkom_mlg.project.xiirpl5102030.belajarmengaji.model.modyel;
+
 public class IqroPage extends AppCompatActivity {
+
+    ArrayList<modyel> mList = new ArrayList<>();
+    adapterr mAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iqro_page);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl5102030/belajarmengaji/model/modyel.java
@@ -0,0 +1,14 @@
+package id.sch.smktelkom_mlg.project.xiirpl5102030.belajarmengaji.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by LordARThur on 19/11/2016.
+ */
+public class modyel {
+    public Drawable foto;
+
+    public modyel(Drawable foto) {
+        this.foto = foto;
+    }
+}
