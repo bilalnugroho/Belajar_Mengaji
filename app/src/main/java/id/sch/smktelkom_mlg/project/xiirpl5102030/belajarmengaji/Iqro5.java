31548498e0691ac4f22ad8147816e78737405ec8
Ratri <Pratiwirespatiningratri@gmail.com>
2016-11-20 11:51:57 +0700

    Commit Intent grid ke iqro

---
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl5102030/belajarmengaji/Iqro5.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.project.xiirpl5102030.belajarmengaji;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Iqro5 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_iqro5);
+    }
+}
--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl5102030/belajarmengaji/IqroPage.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl5102030/belajarmengaji/IqroPage.java
@@ -1,8 +1,10 @@
 package id.sch.smktelkom_mlg.project.xiirpl5102030.belajarmengaji;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 
 import java.util.ArrayList;
 
@@ -20,6 +22,54 @@ public class IqroPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_iqro_page);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        findViewById(R.id.btniq1).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IqroPage.this, Iqro1.class));
+            }
+
+        });
+
+        findViewById(R.id.btniq2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IqroPage.this, Iqro2.class));
+            }
+
+        });
+
+        findViewById(R.id.btniq3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IqroPage.this, Iqro3.class));
+            }
+
+        });
+
+        findViewById(R.id.btniq4).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IqroPage.this, Iqro4.class));
+            }
+
+        });
+
+        findViewById(R.id.btniq5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IqroPage.this, Iqro5.class));
+            }
+
+        });
+
+        findViewById(R.id.btniq6).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IqroPage.this, Iqro6.class));
+            }
+
+        });
     }
 
 
